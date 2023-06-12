248102b99902175f81b7990e1f5e80820a8db4de
Daniel de Souza Cordeiro <dcordeiro962@gmail.com>
2023-06-12 15:53:34 -0300

    [feat]: function for calculator results, consoles witch infos

--- ts/index.ts
+++ ts/index.ts
@@ -1,41 +1,86 @@
-class Numbers {
+class Inputs {
     input1: number = 0;
     input2: number = 0;
 }
 
-const valor = new Numbers();
+const valors = new Inputs();
 
-function numberSelected(numberSelected: string) { 
+let valorContentScreen = document.querySelector(".content-screen");
 
-    var test2 = numberSelected;
-    var test = test2 + numberSelected;
+let arrayValors: string[] = [];
+let valorRemovedFromArray: string = '';
 
+function numberSelected(numberSelected: string) {
 
-    console.log("valor inicial: ",test2);
-    console.log('valor final: ', test);
-    
+    this.arrayValors.push(numberSelected);
+    this.valorRemovedFromArray = this.arrayValors.join("");
+
+    console.log('valor inicial em array de texto: ', this.arrayValors);
+    console.log('valor final em texto: ', this.valorRemovedFromArray);
 }
 
-function operations() {
-   console.log("Zerar operação");   
+let sinal: string = '';
+
+function operations(sinalSelected: string) {
+    if (valors.input1 == 0) {
+        valors.input1 = Number(this.valorRemovedFromArray);
+    }
+
+    console.log('valor salvo no input1 em número: ', valors.input1);  
+
+    this.arrayValors = [];
+    
+    this.sinal = sinalSelected;
+
+    console.log('O sinal selecionado foi: ', this.sinal);
 }
 
 class Calculator {
-    Sum(valor: Numbers): number {         
-        return valor.input1 + valor.input2;
+    Sum(valors: Inputs): number {
+        const resultSum = valors.input1 + valors.input2;    
+        console.log("Resultado da Soma: ", resultSum);
+             
+        return resultSum;
     }
 
-    Subtraction(valor: Numbers): number { 
-        return valor.input1 - valor.input2;
+    Subtraction(valors: Inputs): number { 
+        const resultSubtraction = valors.input1 - valors.input2;
+        console.log("Resultado da Subtração: ", resultSubtraction);        
+
+        return resultSubtraction;
     }
 
-    Multiplication(valor: Numbers): number { 
-        return valor.input1 * valor.input2;
+    Multiplication(valors: Inputs): number {
+        const resultMultiplication = valors.input1 * valors.input2; 
+        console.log("Resultado da Multiplicação: ", resultMultiplication);
+
+        return resultMultiplication;
     }
 
-    Division(valor: Numbers): number { 
-        return valor.input1 / valor.input2;
+    Division(valors: Inputs): number {
+        const resultDivision = valors.input1 / valors.input2;
+        console.log("Resultado da Divisão: ", resultDivision);
+         
+        return resultDivision;
     }
 }
 
 const calculator = new Calculator();
+
+function result() {
+    if (valors.input2 == 0) {
+        valors.input2 = Number(this.valorRemovedFromArray);
+    }
+
+    console.log('valor salvo no input2 em número: ', valors.input2);
+
+    if (this.sinal === '+') {
+        calculator.Sum(valors);
+    } else if (this.sinal === '-') {
+        calculator.Subtraction(valors);
+    } else if (this.sinal === '*') {
+        calculator.Multiplication(valors);
+    } else if (this.sinal === '/') {
+        calculator.Division(valors);
+    }
+}
