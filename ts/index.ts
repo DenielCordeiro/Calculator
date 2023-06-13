7aeb50a74c6661d87cacc81649c39d303bd5ff9d
Daniel de Souza Cordeiro <dcordeiro962@gmail.com>
2023-06-13 13:28:38 -0300

    [feat]: show values in content screen, functions reset

--- ts/index.ts
+++ ts/index.ts
@@ -1,3 +1,5 @@
+var valorInScreen = document.querySelector(".content-screen");
+
 class Inputs {
     input1: number = 0;
     input2: number = 0;
@@ -5,18 +7,18 @@ class Inputs {
 
 const valors = new Inputs();
 
-let valorContentScreen = document.querySelector(".content-screen");
-
 let arrayValors: string[] = [];
 let valorRemovedFromArray: string = '';
 
 function numberSelected(numberSelected: string) {
-
     this.arrayValors.push(numberSelected);
     this.valorRemovedFromArray = this.arrayValors.join("");
 
-    console.log('valor inicial em array de texto: ', this.arrayValors);
-    console.log('valor final em texto: ', this.valorRemovedFromArray);
+    if (valors.input1 == 0) {
+        this.valorInScreen.innerHTML = this.valorRemovedFromArray
+    } else {
+        this.valorInScreen.innerHTML = valors.input1 + '   ' + this.sinal + '   ' + this.valorRemovedFromArray;
+    }
 }
 
 let sinal: string = '';
@@ -25,42 +27,34 @@ function operations(sinalSelected: string) {
     if (valors.input1 == 0) {
         valors.input1 = Number(this.valorRemovedFromArray);
     }
-
-    console.log('valor salvo no input1 em número: ', valors.input1);  
-
-    this.arrayValors = [];
     
     this.sinal = sinalSelected;
-
-    console.log('O sinal selecionado foi: ', this.sinal);
+    this.valorInScreen.innerHTML = valors.input1 + '  ' + this.sinal;
+    this.valuesReset();
 }
 
 class Calculator {
     Sum(valors: Inputs): number {
-        const resultSum = valors.input1 + valors.input2;    
-        console.log("Resultado da Soma: ", resultSum);
-             
+        const resultSum = valors.input1 + valors.input2;  
+
         return resultSum;
     }
 
     Subtraction(valors: Inputs): number { 
-        const resultSubtraction = valors.input1 - valors.input2;
-        console.log("Resultado da Subtração: ", resultSubtraction);        
+        const resultSubtraction = valors.input1 - valors.input2; 
 
         return resultSubtraction;
     }
 
     Multiplication(valors: Inputs): number {
         const resultMultiplication = valors.input1 * valors.input2; 
-        console.log("Resultado da Multiplicação: ", resultMultiplication);
 
         return resultMultiplication;
     }
 
     Division(valors: Inputs): number {
         const resultDivision = valors.input1 / valors.input2;
-        console.log("Resultado da Divisão: ", resultDivision);
-         
+
         return resultDivision;
     }
 }
@@ -72,15 +66,44 @@ function result() {
         valors.input2 = Number(this.valorRemovedFromArray);
     }
 
-    console.log('valor salvo no input2 em número: ', valors.input2);
-
-    if (this.sinal === '+') {
-        calculator.Sum(valors);
+    if (this.sinal === '+') { 
+        this.valorInScreen.innerHTML = calculator.Sum(valors);
     } else if (this.sinal === '-') {
-        calculator.Subtraction(valors);
+        this.valorInScreen.innerHTML = calculator.Subtraction(valors);
     } else if (this.sinal === '*') {
-        calculator.Multiplication(valors);
+        this.valorInScreen.innerHTML = calculator.Multiplication(valors);
     } else if (this.sinal === '/') {
-        calculator.Division(valors);
+        this.valorInScreen.innerHTML = calculator.Division(valors);
+    } else {
+        alert("[ERROR !]: no operation signal has been entered");
     }
+
+    this.resetInputsAndSinal();
+    this.valuesReset();
+}
+
+function valuesReset(): string {
+    const messageReset = "values reset";
+    this.arrayValors = [];
+    
+    return messageReset;
+}
+
+function resetInputsAndSinal(): Inputs {
+    valors.input1 = 0;
+    valors.input2 = 0;
+
+    this.sinal = '';
+
+    return valors;
+}
+
+function toCleanScreenContent(): string {
+    const messageClear = "Você limpou o visor";
+
+    this.resetInputsAndSinal();
+    this.valuesReset();
+    this.valorInScreen.innerHTML = '';
+    
+    return messageClear;
 }
