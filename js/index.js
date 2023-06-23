1b0b0cced516a8975f3eef1206d942aacc7f6317
Daniel de Souza Cordeiro <dcordeiro962@gmail.com>
2023-06-23 14:05:22 -0300

    [feat]: manage to save last account

--- js/index.js
+++ js/index.js
@@ -1,114 +1,96 @@
 var valorInScreen = document.querySelector(".content-screen");
-
-class Inputs {
-    input1: number = 0;
-    input2: number = 0;
-}
-
-const valors = new Inputs();
-
-let arrayValors: string[] = [];
-let valorRemovedFromArray: string = '';
-
-function numberSelected(numberSelected: string) {
+var Inputs = /** @class */ (function () {
+    function Inputs() {
+        this.input1 = 0;
+        this.input2 = 0;
+    }
+    return Inputs;
+}());
+var valors = new Inputs();
+var arrayValors = [];
+var valorRemovedFromArray = '';
+function numberSelected(numberSelected) {
     this.arrayValors.push(numberSelected);
     this.valorRemovedFromArray = this.arrayValors.join("");
-
     if (valors.input1 == 0) {
-        this.valorInScreen.innerHTML = this.valorRemovedFromArray
-    } else {
+        this.valorInScreen.innerHTML = this.valorRemovedFromArray;
+    }
+    else {
         this.valorInScreen.innerHTML = valors.input1 + '   ' + this.sinal + '   ' + this.valorRemovedFromArray;
     }
 }
-
-let sinal: string = '';
-
-function operations(sinalSelected: string) {
+var sinal = '';
+function operations(sinalSelected) {
     if (valors.input1 == 0) {
         valors.input1 = Number(this.valorRemovedFromArray);
     }
-    
     this.sinal = sinalSelected;
     this.valorInScreen.innerHTML = valors.input1 + '  ' + this.sinal;
     this.valuesReset();
 }
-
-class Calculator {
-    Sum(valors: Inputs): number {
-        const resultSum = valors.input1 + valors.input2;  
-
+var Calculator = /** @class */ (function () {
+    function Calculator() {
+    }
+    Calculator.prototype.Sum = function (valors) {
+        var resultSum = valors.input1 + valors.input2;
         return parseFloat(resultSum.toFixed(2));
-    }
-
-    Subtraction(valors: Inputs): number { 
-        const resultSubtraction = valors.input1 - valors.input2; 
-
+    };
+    Calculator.prototype.Subtraction = function (valors) {
+        var resultSubtraction = valors.input1 - valors.input2;
         return parseFloat(resultSubtraction.toFixed(2));
-    }
-
-    Multiplication(valors: Inputs): number {
-        const resultMultiplication = valors.input1 * valors.input2; 
-
+    };
+    Calculator.prototype.Multiplication = function (valors) {
+        var resultMultiplication = valors.input1 * valors.input2;
         return parseFloat(resultMultiplication.toFixed(2));
-    }
-
-    Division(valors: Inputs): number {
-        const resultDivision = valors.input1 / valors.input2;
-
+    };
+    Calculator.prototype.Division = function (valors) {
+        var resultDivision = valors.input1 / valors.input2;
         return parseFloat(resultDivision.toFixed(2));
-    }
-}
-
-const calculator = new Calculator();
-
+    };
+    return Calculator;
+}());
+var calculator = new Calculator();
 function result() {
     if (valors.input2 == 0) {
         valors.input2 = Number(this.valorRemovedFromArray);
     }
-
-    if (this.sinal === '+') { 
+    if (this.sinal === '+') {
         this.valorInScreen.innerHTML = calculator.Sum(valors);
         valors.input1 = calculator.Sum(valors);
-    } else if (this.sinal === '-') {
+    }
+    else if (this.sinal === '-') {
         this.valorInScreen.innerHTML = calculator.Subtraction(valors);
         valors.input1 = calculator.Subtraction(valors);
-    } else if (this.sinal === '*') {
+    }
+    else if (this.sinal === '*') {
         this.valorInScreen.innerHTML = calculator.Multiplication(valors);
         valors.input1 = calculator.Multiplication(valors);
-    } else if (this.sinal === '/') {
+    }
+    else if (this.sinal === '/') {
         this.valorInScreen.innerHTML = calculator.Division(valors);
         valors.input1 = calculator.Division(valors);
-    } else {
+    }
+    else {
         alert("[ERROR !]: no operation signal has been entered");
     }
-
     this.resetInputsAndSinal();
     this.valuesReset();
 }
-
-function valuesReset(): string {
-    const messageReset = "values reset";
+function valuesReset() {
+    var messageReset = "values reset";
     this.arrayValors = [];
-    
     return messageReset;
 }
-
-function resetInputsAndSinal(): Inputs {
+function resetInputsAndSinal() {
     valors.input2 = 0;
-
     this.sinal = '';
-
     return valors;
 }
-
-function toCleanScreenContent(): string {
-    const messageClear = "Você limpou o visor";
-
+function toCleanScreenContent() {
+    var messageClear = "Você limpou o visor";
     this.resetInputsAndSinal();
     this.valuesReset();
     this.valorInScreen.innerHTML = '';
-
     valors.input1 = 0;
-    
     return messageClear;
 }
